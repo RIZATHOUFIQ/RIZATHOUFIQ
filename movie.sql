9cf312b07a1762aa4dbf470eb624b176dc8ef9e4
RIZATHOUFIQ <108537386+RIZATHOUFIQ@users.noreply.github.com>
2022-07-01 20:37:56 +0530

    Add files via upload

---
+++ movie.sql
@@ -0,0 +1,5 @@
+create table movie(movie_name varchar(20),lead_actor_name varchar(20),actress_name varchar(20),year_of_releas INTEGER,director_name varchar(20))
+insert into movie values("JanaGanaMana","Prithiviraj","MamtaMohanadas",2022,"DijoJOseAntony");
+insert into movie values("Master","Vijay","MalavikaMohan",2021,"Lokesh");
+insert into movie values("Suzhal","Kathir","AishwaryaRajesh",2022,"Pushkar");
+insert into movie values("Karnan","Dhanush","RajishaVijayam",2020,"MariSelvaraj");
